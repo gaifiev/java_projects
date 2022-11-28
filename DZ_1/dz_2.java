cb69d2ca903b582a17778110fc04e38fcc90a800
gaifiev <v.gaifiev@mail.ru>
2022-11-28 23:48:21 +0300

    add dz1 3 solutions

--- DZ_1/dz_2.java
+++ DZ_1/dz_2.java
@@ -3,7 +3,7 @@ package DZ_1;
 
 public class dz_2 {
     public static void main(String[] args) {
-        System.out.println("Простые числа от 1 до 1000: "); // вывод в терминал
+        System.out.println("Все простые числа от 1 до 1000: "); // вывод в терминал
         for (int i = 2; i <= 1000; i++) { // перебирает массив или числа
             for (int j = 2; j <= i; j++) {
                 if (i % j == 0) { // делит числа сами на себя
