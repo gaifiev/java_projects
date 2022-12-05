7bc5aaa49b2e7f1632d80d3494a6a4fde5a12622
gaifiev <v.gaifiev@mail.ru>
2022-12-06 01:25:48 +0300

    add dz3

--- DZ_3/dz_3.java
+++ DZ_3/dz_3.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class dz_3 {
-
     public static void main(String[] args) throws IOException {
         int input = 0;
         int input1 = 0;
@@ -17,6 +16,9 @@ public class dz_3 {
         array.add(3);
         array.add(9);
         array.add(15);
+        array.add(11);
+        array.add(10);
+        array.add(16);
 
         for (int j : array) {
             input += j;
@@ -26,7 +28,5 @@ public class dz_3 {
         System.out.printf("Среднее арифмитическое: %d\n", i);
         System.out.printf("Максимальное: %d\n", Collections.max(array));
         System.out.printf("Минимальное: %d\n", Collections.min(array));
-
     }
-
 }
